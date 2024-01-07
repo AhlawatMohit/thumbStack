c411b687300b60b61e4b8701c98c92d17d715279
Mohit Ahlawat <mohitahlawat223@gmail.com>
2024-01-08 00:49:38 +0530

    thumbStack Assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+let value  = new Typed("#text", {
+    strings: [ "Frontend Developer", "MERN Stack", "Web Developer" ],
+    typeSpeed: 100,
+    backSpeed: 120,
+    backDelay: 1000,
+    loop: true 
+})
